5cf652a2e0093c68d2519f347d314e50391106d4
stevennvl <126872705+stevennvl@users.noreply.github.com>
2024-03-23 00:51:21 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:daftar_belanja/screens/shopping_list_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Daftar Belanja',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ShoppingListScreen(),
+    );
+  }
+}
---
+++ lib/screens/shopping_list_screen.dart
@@ -0,0 +1,82 @@
+import 'package:daftar_belanja/services/shopping_service.dart';
+import 'package:flutter/material.dart';
+
+class ShoppingListScreen extends StatefulWidget {
+  const ShoppingListScreen({super.key});
+
+  @override
+  State<ShoppingListScreen> createState() => _ShoppingListScreenState();
+}
+
+class _ShoppingListScreenState extends State<ShoppingListScreen> {
+  final TextEditingController _controller = TextEditingController();
+  final ShoppingService _shoppingService = ShoppingService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Daftar Belanja"),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration:
+                        const InputDecoration(hintText: "Masukan Nama Barang"),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () {
+                    _shoppingService.addShoppingItem(_controller.text);
+                    _controller.clear();
+                  },
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: StreamBuilder<Map<String, String>>(
+              stream: _shoppingService.getShoppingList(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  Map<String, String> items = snapshot.data!;
+                  return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      final key = items.keys.elementAt(index);
+                      final item = items[key];
+                      return ListTile(
+                        title: Text(item!),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () {
+                            _shoppingService.removeShoppingItem(key);
+                          },
+                        ),
+                      );
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text("Error: ${snapshot.error}"),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
